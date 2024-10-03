ca2bcee9a457ece8edeaa576faaff90007b12704
Mkien <manhkien1005@gmail.com>
2024-10-03 09:15:35 +0700

    fix update room count function

--- src/api/hotel/controllers/hotel-update.js
+++ src/api/hotel/controllers/hotel-update.js
@@ -1,23 +1,26 @@
 module.exports = {
   async updateRoomCount(ctx) {
     const { hotelId, event } = ctx.request.body;
+    console.log(hotelId, event);
     if (event === "room-added") {
       if (!hotelId) {
         return ctx.badRequest("Hotel ID is required");
       }
 
       // Lấy thông tin khách sạn và cập nhật số lượng phòng
-      const hotel = await strapi.services.hotel.findOne({ id: hotelId });
+      const hotel = await strapi.db.query("api::hotel.hotel").findOne({
+        where: { id: hotelId },
+      });
 
       if (!hotel) {
         return ctx.notFound("Hotel not found");
       }
 
       // Tăng số lượng phòng của khách sạn
-      const updatedHotel = await strapi.services.hotel.update(
-        { id: hotelId },
-        { rooms: hotel.rooms + 1 },
-      );
+      const updatedHotel = await strapi.db.query("api::hotel.hotel").update({
+        where: { id: hotelId },
+        data: { rooms: parseInt(hotel.rooms) + 1 },
+      });
 
       return ctx.send(updatedHotel);
     }
@@ -25,16 +28,18 @@ module.exports = {
       if (!hotelId) {
         return ctx.badRequest("Hotel ID is required");
       }
-      const hotel = await strapi.services.hotel.findOne({ id: hotelId });
+      const hotel = await strapi.db.query("api::hotel.hotel").findOne({
+        where: { id: hotelId },
+      });
 
       if (!hotel) {
         return ctx.notFound("Hotel not found");
       }
 
-      const updatedHotel = await strapi.services.hotel.update(
-        { id: hotelId },
-        { rooms: hotel.rooms - 1 },
-      );
+      const updatedHotel = await strapi.db.query("api::hotel.hotel").update({
+        where: { id: hotelId },
+        data: { rooms: parseInt(hotel.rooms) - 1 },
+      });
 
       return ctx.send(updatedHotel);
     }
