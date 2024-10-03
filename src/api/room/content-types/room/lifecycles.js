e8ac284e8c24c9ba708d254503a379604b4716da
Mkien <manhkien1005@gmail.com>
2024-10-03 10:29:10 +0700

    room service init

---
+++ src/api/room/content-types/room/lifecycles.js
@@ -0,0 +1,42 @@
+const axios = require("axios");
+
+module.exports = {
+  async afterCreate(event) {
+    console.log(
+      "afterCreate lifecycle triggered for Room ID:",
+      event.result.id
+    );
+
+    try {
+      await axios.post("https://hotel-service-ac92.onrender.com/api/events", {
+        event: "room-added",
+        roomId: event.result.id,
+        hotelId: event.result.hotel_id,
+      });
+      console.log("Room added event sent to Hotel Service");
+    } catch (error) {
+      console.error("Error sending room-added event to Hotel Service:", error);
+    }
+  },
+
+  async afterDelete(event) {
+    console.log(
+      "afterDelete lifecycle triggered for Room ID:",
+      event.result.id
+    );
+
+    try {
+      await axios.post("https://hotel-service-ac92.onrender.com/api/events", {
+        event: "room-removed",
+        roomId: event.result.id,
+        hotelId: event.result.hotel_id,
+      });
+      console.log("Room removed event sent to Hotel Service");
+    } catch (error) {
+      console.error(
+        "Error sending room-removed event to Hotel Service:",
+        error
+      );
+    }
+  },
+};
