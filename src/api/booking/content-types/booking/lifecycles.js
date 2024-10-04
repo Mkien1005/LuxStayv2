04be87138ad02bf887b9eb9e64049eab0bfe1848
Mkien <manhkien1005@gmail.com>
2024-10-04 07:29:51 +0700

    add lifecycle booking

---
+++ src/api/booking/content-types/booking/lifecycles.js
@@ -0,0 +1,34 @@
+module.exports = {
+  async afterUpdate(event) {
+    let status = event.result.status;
+    if (status === "Đã thanh toán") {
+      let roomId = event.result.room_id;
+      let checkIn = event.result.check_in;
+      let checkOut = event.result.check_out;
+
+      let room = await strapi.plugins["axios"].request({
+        method: "GET",
+        url: `https://room-service-q9pa.onrender.com/api/rooms/${roomId}`,
+      });
+      let check_in_date = room.data.attributes.check_in_date;
+      check_in_date = [
+        ...check_in_date,
+        {
+          check_in: checkIn,
+          check_out: checkOut,
+        },
+      ];
+
+      await strapi.plugins["axios"].request({
+        method: "PUT",
+        url: `https://room-service-q9pa.onrender.com/api/rooms/${roomId}`,
+        data: {
+          data: {
+            check_in_date: check_in_date,
+          },
+        },
+      });
+      console.log("Đã cập nhật trạng thái phòng thành công");
+    }
+  },
+};
