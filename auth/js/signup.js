e0ce27f9f7a0691ee8ebebc54c6d8ea3168b5fe1
Mkien <manhkien1005@gmail.com>
2024-11-01 22:10:52 +0700

    fix & bugs

--- auth/js/signup.js
+++ auth/js/signup.js
@@ -1,4 +1,45 @@
-
+let username = document.getElementById('username')
+let errorUsername = document.getElementById('errorUsername')
+let password = document.getElementById('password')
+let errorPass = document.getElementById('errorPass')
+let email = document.getElementById('email')
+let errorEmail = document.getElementById('errorEmail')
+let signUp = document.querySelector('signup-button')
+username.addEventListener('change', () => {
+  if (username.value.length < 3) {
+    errorUsername.textContent = 'Username must be at least 3 characters long'
+    signUp.disabled = true
+  } else {
+    errorUsername.textContent = ''
+    signUp.disabled = false
+  }
+})
+password.addEventListener('change', () => {
+  if (password.value.length < 8) {
+    errorPass.textContent = 'Password must be at least 8 characters long'
+    signUp.disabled = true
+  } else {
+    errorPass.textContent = ''
+    signUp.disabled = false
+  }
+})
+email.addEventListener('change', () => {
+  let regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+  if (!regexEmail.test(email.value)) {
+    errorEmail.textContent = 'Invalid email address'
+    signUp.disabled = true
+  } else {
+    errorEmail.textContent = ''
+    signUp.disabled = false
+  }
+  if (email.value.length < 3) {
+    errorEmail.textContent = 'Email must be at least 3 characters long'
+    signUp.disabled = true
+  } else {
+    errorEmail.textContent = ''
+    signUp.disabled = false
+  }
+})
 let signupbt = document.querySelector('.signup-button')
 signupbt.addEventListener('click', (e) => {
   e.preventDefault()
@@ -13,7 +54,7 @@ signupbt.addEventListener('click', (e) => {
   const registerData = {
     username: username,
     email: email,
-    password: password
+    password: password,
   }
 
   fetch('https://luxstayv2.onrender.com/identity/users', {
@@ -27,32 +68,32 @@ signupbt.addEventListener('click', (e) => {
       if (response.ok) {
         // Đăng ký thành công, gửi yêu cầu đăng nhập ngay sau đó
         Swal.fire({
-            icon: 'success',
-            title: 'Đăng ký thành công',
-            showConfirmButton: false,
-            timer: 2000,
-          })
-          setTimeout(() => {
-            window.location.href = '../auth/login.html'
-          }, 2000)
+          icon: 'success',
+          title: 'Đăng ký thành công',
+          showConfirmButton: false,
+          timer: 2000,
+        })
+        setTimeout(() => {
+          window.location.href = '../auth/login.html'
+        }, 2000)
       } else {
         return response.json().then((data) => {
-            console.log(data)
+          console.log(data)
           // Kiểm tra nếu có message trong phản hồi
           if (data && data.message) {
             Swal.fire({
-                icon: 'error',
-                title: data.message,
-                showConfirmButton: false,
-                timer: 2000,
-              })
+              icon: 'error',
+              title: data.message,
+              showConfirmButton: false,
+              timer: 2000,
+            })
           } else {
             Swal.fire({
-                icon: 'error',
-                title: 'Có lỗi xảy ra, vui lòng thử lại.',
-                showConfirmButton: false,
-                timer: 2000,
-              })
+              icon: 'error',
+              title: 'Có lỗi xảy ra, vui lòng thử lại.',
+              showConfirmButton: false,
+              timer: 2000,
+            })
           }
         })
       }
